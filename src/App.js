2d8f6a1c47285ec0aed9a507c2636b1f5864c146
mmarew <35652680+mmarew@users.noreply.github.com>
2022-12-29 04:23:42 +0300

    remove comments

--- src/App.js
+++ src/App.js
@@ -18,14 +18,7 @@ function App() {
       {TitleAndRequest.map((data) => {
         return <Row title={data.title} fetchUrl={data.fetchUrl} />;
       })}
-      {/*      
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horrer Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} /> */}
+      
     </div>
   );
 }
